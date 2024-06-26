af78de43f666d630b2cd07c4424457843beef801
Ackel-den(Denis) <gamedenis0@gmail.com>
2024-06-26 19:34:25 +0500

    ааа

--- MemorizeHW/ContentView.swift
+++ MemorizeHW/ContentView.swift
@@ -70,31 +70,7 @@ struct NewGameButton: View {
     }
 }
 
-struct CardView: View {
-    let card: MemoryGame<String>.Card
-    let color: Color
 
-    var body: some View {
-        ZStack{
-            let shape = RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-            if card.isFaceUp{
-                shape.fill()
-                shape.foregroundColor(color)
-                shape.strokeBorder(lineWidth: 3)
-                Text(card.content).font(.largeTitle)
-            }
-            else if card.isMatched{
-                shape.opacity(0)
-            }
-            else{
-                shape.fill()
-                shape.foregroundColor(color)
-                shape.strokeBorder(lineWidth: 3)
-            }
-            
-        }
-    }
-}
 
 #Preview {
     let game = ViewModel()
